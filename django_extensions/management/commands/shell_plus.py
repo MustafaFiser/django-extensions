MustafaFiser/django-extensions

commit 82b875fe8f806d00b6ccc5521712a8bfd425a006

Display execution time after query in shell_plus --print-sql

django_extensions/management/commands/shell_plus.py
@@ -1,6 +1,7 @@
 import os
 from django.core.management.base import NoArgsCommand
 from optparse import make_option
+from datetime import datetime
 
 
 class Command(NoArgsCommand):
@@ -41,14 +42,18 @@ class Command(NoArgsCommand):
 
             class PrintQueryWrapper(util.CursorDebugWrapper):
                 def execute(self, sql, params=()):
+                    starttime = datetime.now()
                     try:
                         return self.cursor.execute(sql, params)
                     finally:
                         raw_sql = self.db.ops.last_executed_query(self.cursor, sql, params)
+                        execution_time = datetime.now() - starttime
                         if sqlparse:
                             print sqlparse.format(raw_sql, reindent=True)
                         else:
                             print raw_sql
+                        print
+                        print 'Execution time: %fs' % execution_time.total_seconds()
                         print
 
             util.CursorDebugWrapper = PrintQueryWrapper

commit 75bfab98aa3d5c088fb01eaeb58fa23a1526ca91

Fix failure in python 2.6 when using "shell_plus --print-sql"
Change introduced in [82b875f] to display sql execution time relies on a method of datetime.timedelta introduced in python 2.7 (total_seconds). Using time.time() is simpler and backwards compatible.

django_extensions/management/commands/shell_plus.py
@@ -1,7 +1,7 @@
 import os
 from django.core.management.base import NoArgsCommand
 from optparse import make_option
-from datetime import datetime
+from time import time
 
 
 class Command(NoArgsCommand):
@@ -42,18 +42,18 @@ class Command(NoArgsCommand):
 
             class PrintQueryWrapper(util.CursorDebugWrapper):
                 def execute(self, sql, params=()):
-                    starttime = datetime.now()
+                    starttime = time()
                     try:
                         return self.cursor.execute(sql, params)
                     finally:
                         raw_sql = self.db.ops.last_executed_query(self.cursor, sql, params)
-                        execution_time = datetime.now() - starttime
+                        execution_time = time() - starttime
                         if sqlparse:
                             print sqlparse.format(raw_sql, reindent=True)
                         else:
                             print raw_sql
                         print
-                        print 'Execution time: %fs' % execution_time.total_seconds()
+                        print 'Execution time: %.6fs' % execution_time
                         print
 
             util.CursorDebugWrapper = PrintQueryWrapper